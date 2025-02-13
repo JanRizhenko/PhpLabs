f703d4d4defc7060c3868baf1ea57860762af57d
JanRizhenko <ipz231_ryav@student.ztu.edu.ua>
2025-02-13 13:28:10 +0200

    Lab01 1.5/2

---
+++ Lab01/Task3/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task3</title>
+    <style>
+        *
+        {
+            margin-top: 50px;
+            text-align: center;
+            font-size: 26px;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+    $month = 2;
+    if ($month == 12 || $month == 1 || $month == 2)
+        {
+            echo("It's winter outside!");
+        }
+    else if ($month == 3 || $month == 4 || $month == 5 )
+    {
+        echo("It's spring outside!");
+    }
+    else if ($month == 6 || $month == 7 || $month == 8 )
+    {
+        echo("It's summer outside!");
+    }
+    else if ($month == 9 || $month == 10 || $month == 11 )
+    {
+        echo("It's autumn outside!");
+    }
+    else
+    {
+        echo("Number should be less than 12 and greater than 1");
+    }
+?>
+
+</body>
+</html>
---
+++ Lab01/Task4/Index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task4</title>
+
+    <style>
+        *
+        {
+            margin-top: 50px;
+            text-align: center;
+            font-size: 26px;
+        }
+    </style>
+</head>
+<body>
+
+<?php
+$char = 'b';
+
+$char = strtolower($char);
+
+switch ($char) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'y':
+        echo "Символ '$char' є голосним.";
+        break;
+    default:
+        if (ctype_alpha($char)) {
+            echo "Символ '$char' є приголосним.";
+        } else {
+            echo "Введений символ '$char' не є літерою.";
+        }
+        break;
+}
+?>
+
+</body>
+</html>
