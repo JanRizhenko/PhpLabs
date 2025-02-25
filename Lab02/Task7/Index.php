b91f273ad15e1b598a407d1752bb64b740d6d311
JanRizhenko <ipz231_ryav@student.ztu.edu.ua>
2025-02-25 21:26:54 +0200

    Lab02 version 0.65

--- Lab02/Task7/Index.php
+++ Lab02/Task7/Index.php
@@ -10,7 +10,44 @@
 <body>
 
 <?php
-    
+    function createArray()
+    {
+        $array = [];
+        for ($i = 0; $i <= random_int(3,7); $i++)
+        {
+            $array[$i] = random_int(10,20);
+        }
+        return $array;
+    }
+    function mainFunction($array1, $array2)
+    {
+        $mergedArray = array_merge($array1, $array2);
+        $sortedArray = array_unique($mergedArray);
+        sort($sortedArray);
+        return $sortedArray;
+    }
+
+
+    $array1 = createArray();
+    echo "Згенерований масив 1: <br>";
+    foreach ($array1 as $item)
+    {
+        echo "$item ";
+    }
+    echo "<br>";
+    $array2 = createArray();
+    echo "Згенерований масив 2: <br>";
+    foreach ($array2 as $item)
+    {
+        echo "$item ";
+    }
+    echo "<br>";
+    $sortedArray = mainFunction($array1, $array2);
+    echo "Поєднаний та відсортований масив: <br>";
+    foreach ($sortedArray as $item)
+    {
+        echo "$item ";
+    }
 ?>
 
 </body>
