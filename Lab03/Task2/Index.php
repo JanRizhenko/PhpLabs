51dbdd907169c6b4b0ef9ae23af946da9f3f5195
JanRizhenko <ipz231_ryav@student.ztu.edu.ua>
2025-03-10 18:07:39 +0200

    Lab02 version 0.44

---
+++ Lab03/Task2/Index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task 2</title>
+</head>
+<body>
+
+<form method="post">
+    <label for="login"></label>
+    <input name="login" type="text">
+    <label for="password"></label>
+    <input name="password" type="password">
+</form>
+
+<?php
+
+?>
+
+</body>
+</html>
